e3db585a616cce2dd6d1f404cab93296ebb84152
Matias Sendra <mjs.sendra@gmail.com>
2024-07-02 00:27:32 -0300

    Final commit

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
     templateUrl: './login-page.component.html',
@@ -10,9 +15,25 @@ export class LoginPageComponent {
 
 
     private fb = inject(FormBuilder);
+    private authService = inject(AuthService);
+    private router = inject(Router)
+
 
     public myForm: FormGroup = this.fb.group({
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
-    })
+        email: ['mjs.sendra@gmail.com', [Validators.required, Validators.email]],
+        password: ['123456', [Validators.required, Validators.minLength(6)]],
+    });
+
+
+
+    login() {
+        const { email, password } = this.myForm.value;
+        this.authService.login(email, password)
+            .subscribe({
+                next: () => this.router.navigateByUrl('/dashboard'),
+                error: (message) => {
+                    Swal.fire('Error', message, 'error')
+                }
+            })
+    }
 }
